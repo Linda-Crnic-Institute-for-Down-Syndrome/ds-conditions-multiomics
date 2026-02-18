b5e7f1d9b9d6f1a990ecc7a785cb2a03dd6ac7d4
mattgalbraith <you@example.com>
2026-02-18 16:54:08 -0700

    Adding SOMAscan_T21_BMI/

---
+++ SOMAscan_T21_BMI/SOMAscan_T21_BMI_lm.R
@@ -0,0 +1,418 @@
+################################################################################
+# Title: Differential abundance analysis of T21 plasma proteins vs. BMI
+# Author(s):
+#   - Zenitha Sundararajan
+#   - Matthew Galbraith
+# Affiliation(s):
+#   - Linda Crnic Institute for Down syndrome
+#   - University of Colorado Anschutz
+################################################################################
+
+### Summary: 
+# Linear regression modelling for differential abundance of plasma proteins and
+# measured by the SOMAscan assay in individuals with Down syndrome.
+# See README.md for more details
+
+### Data type(s):
+#   A. Human Trisome Project (HTP) sample meta data 
+#      - HTP_Metadata_v0.5_Synapse.txt
+#   B. HTP SomaScan data
+#      - HTP_SOMAscan_Proteomics_Synapse.txt
+#
+
+### Workflow:
+#   Step 1 - Read in meta data and sample data
+#   Step 2 - Linear regression model setup and assessment
+#   Step 3 - Model results
+#   Step 4 - Visualize results
+#   Step 5 - Gene set enrichment analysis (GSEA)
+#   Step 6 - Visualize GSEA results
+
+### Change Log:
+# v1.0
+# Initial version
+#
+
+
+# 0 General Setup -----
+## initialize renv
+# renv::init(bioconductor = TRUE) # run first time only
+
+## 0.1 Load required libraries ----
+library("readxl") # Used to read .xlsx files
+library("openxlsx") # used for data export as Excel workbooks
+library("tidyverse") # data manipulation, visualization, and tidy workflows
+library("patchwork") # for assembling plot layouts
+library("skimr") # data table summaries
+library("janitor") # data cleaning and other functions
+library("tictoc") # timer
+library("lmerTest") # required to get pvals for mixed model with broom.mixed::tidy()
+library("broom.mixed") # for tidying models
+library("ggplot2") #data visualization
+library("ggrepel") # required for labelling features
+library("conflicted") # force all conflicts to become errors
+conflicts_prefer( # declare preferences in cases of conflict
+  dplyr::filter,
+  dplyr::count,
+  dplyr::rename,
+  dplyr::bind_rows
+)
+library("here") # generates path to current project directory
+#
+
+## 0.2 Set required parameters ----
+#
+# Input data files
+# These files need to be downloaded from ****
+htp_meta_data_file <- here("data", "HTP_Metadata_v0.5_Synapse.txt")
+# SomaScan data file
+somascan_data_file <- here("data", "HTP_SOMAscan_Proteomics_Synapse.txt")
+#
+standard_colors <- c("Control" = "gray60", "T21" = "#009b4e")
+#
+out_file_prefix <- "SOMAscan_T21_BMI_lm.R_" # should match this script title
+# End required parameters ###
+source(here("helper_functions.R")) # load helper functions
+#
+
+
+# 1 Read in meta data and sample data ----
+## 1.1 Read in and inspect HTP metadata  -----
+meta_data <- htp_meta_data_file %>% 
+  read_tsv() %>% #1722 obs/rows
+  filter(Event_name != "Current") %>% 
+  mutate(
+    Sex = fct_relevel(Sex, "Female"),
+    Karyotype = fct_relevel(Karyotype, "Control"),
+    Sample_source = as_factor(Sample_source_code), # convert to factor - default is numerical order
+    BMI = round(Weight_kg/((Height_cm/100) ** 2 ),2 )
+  ) 
+# 
+
+## 1.2 Read in and inspect pre-processed SomaScan data  -----
+somascan_data <- somascan_data_file %>% 
+  read_tsv()  
+# inspect
+somascan_data %>% distinct(LabID) # 419 samples
+somascan_data %>% distinct(Aptamer) # 4632 measurements each for 419 samples = 1,940,808
+#
+
+## 1.3 Merge SomaScan data with meta data  -----
+somascan_data_meta_data <- somascan_data %>%
+  inner_join(meta_data,
+             by = c("LabID")) %>% # keeps 1,940,808 rows
+  filter(!is.na(Weight_kg), !is.na(Height_cm)) # keeps 1,875,960 rows
+#
+
+
+# 2 Linear regression model setup and assessment ----
+## 2.1 Prepare data for linear regression ----
+regressions_data <- somascan_data_meta_data %>% #1,875,960 rows
+  filter(Karyotype == "T21") |>  # keeps 1,454,448 rows
+  select(ParticipantID, LabID, BMI, Sex, Age, Sample_source, Aptamer, GeneSymbol, Value) %>% 
+  mutate(
+    Sex = fct_relevel(Sex, c("Female", "Male")), # ensure factor levels in correct order
+  ) %>% 
+  group_by(Aptamer) %>% # group by aptamer
+  mutate(extreme_value = rstatix::is_extreme(log2(Value))
+  ) %>%
+  ungroup() %>%
+  filter(extreme_value != TRUE) %>% # keeps 1,425,828 rows (extreme outliers are removed per feature)
+  # check here for:
+  # 1) a minimum number of samples per group (eg 5) and 
+  # 2) that there are >1 levels for categorical variables of interest (prevents errors in regression step)
+  group_by(Aptamer) %>%  # CHECK CORRECT GROUPING
+  mutate( # count number of levels for EACH categorical variable
+    Sex_levels = Sex %>% fct_drop() %>% levels() %>% length()
+  ) %>%
+  filter(Sex_levels > 1) %>% # need to require >1 level for each categorical level or lm() gives error
+  ungroup() %>%
+  select(ParticipantID, LabID, BMI, Sex, Age, Sample_source, Aptamer, GeneSymbol, Value) |> 
+  nest(data = c(ParticipantID, LabID, BMI, Sex, Age, Sample_source, Value)) # nesting allows for easy testing of all features ~ at once
+#
+
+
+## 2.2 Linear regression: Multi-model: BMI + Age + Sex + Sample_source ----
+tic("Running linear regressions for multi model with Sex + Age + Sample_source...")
+regressions_multi_AgeSexSourceFixed <- regressions_data %>% 
+  mutate(
+    fit = map(data, ~ lm(log2(Value) ~ BMI +  Age + Sex + Sample_source, data = .x)),
+    tidied = map(fit, broom::tidy), # see ?tidy.lm
+    glanced = map(fit, broom::glance), # see ?glance.lm
+    augmented = map(fit, broom::augment), # see ?augment.lm
+    vifs = map(fit, ~car::vif(mod = .x) %>% as_tibble(rownames = "term"))
+  )
+toc() # ~ 15.613 sec elapsed; no warnings
+#
+
+
+# 3 Model results ----
+## 3.1 Extract results from preferred model: Multi-model: BMI + Age + Sex + Sample_source ----
+lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results <- regressions_multi_AgeSexSourceFixed |> 
+  unnest(tidied) |> 
+  filter(str_detect(term, "BMI")) |>
+  select(Aptamer, GeneSymbol,term, log2FoldChange = estimate, pval = p.value) |> 
+  mutate(FoldChange = 2^log2FoldChange) |> 
+  arrange(pval) %>% 
+  mutate(BHadj_pval = p.adjust(pval, method = "BH", n = length(pval))) %>% 
+  select(
+    Aptamer,
+    GeneSymbol,
+    FoldChange,
+    log2FoldChange,
+    pval,
+    BHadj_pval,
+    everything()
+  )
+#
+lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results %>% 
+  filter(BHadj_pval < 0.1 )
+#
+
+## 3.3 Export the results ----
+lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results %>%
+  write_tsv(file = here("results","HTP T21 bmi plasma proteome linear regressions results.txt"))
+#
+
+
+# 4 Plots ----
+## 4.1 Volcano plots -----
+v_plot <- lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results %>% 
+  volcano_plot_lab_lm(
+    title = "Diff. abundance of plasma proteins (T21 vs. BMI) - linear regression",
+    subtitle = paste0("T21s only; AgeSexSource adjusted model\n[Down: ", (.) %>% filter(BHadj_pval < 0.1 & FoldChange < 1) %>% nrow(), "; Up: ", (.) %>% filter(BHadj_pval < 0.1 & FoldChange > 1) %>% nrow(), "]")
+  )
+v_plot
+# save the plot
+ggsave(filename = here("plots", paste0(out_file_prefix, "lm_volcano_plot_sig", ".pdf")), device = cairo_pdf, width = 5, height = 5, units = "in")
+
+
+## 4.2 Scatter plots of top associations ----
+# Adjust the dataset for covariates to align with the linear regression model
+### 4.2.1 Adjust the SomaScan dataset ----
+somascan_sample_data <-  somascan_data_meta_data %>% #1,875,960 rows
+  select(-Value) %>%
+  distinct()
+#
+somascan_unadj_data <- somascan_data_meta_data %>% #1,875,960 rows
+  select(LabID, Aptamer, Value) %>%
+  mutate(Value = log2(Value)) %>% # need to log2 transform for batch correction
+  pivot_wider(names_from = LabID, values_from = Value) %>%
+  column_to_rownames(var = "Aptamer")
+#
+somascan_source_vec <- somascan_unadj_data %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>%
+  inner_join(
+    meta_data %>% select(LabID, Sample_source)
+  ) %>%
+  pull(Sample_source)
+#
+somascan_Sex_vec <- somascan_unadj_data %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>%
+  inner_join(
+    meta_data %>% select(LabID, Sex)
+  ) %>%
+  pull(Sex)
+#
+somascan_Age_vec <- somascan_unadj_data %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>%
+  inner_join(
+    meta_data %>% select(LabID, Age) 
+  ) %>%
+  pull(Age)
+#
+somascan_design <- somascan_unadj_data %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>%
+  inner_join(
+    meta_data %>% select(LabID, Karyotype)
+  ) %>%
+  model.matrix(~ Karyotype, data = .)
+#
+somascan_SourceSexAge_adj <- somascan_unadj_data %>%
+  limma::removeBatchEffect(
+    batch = somascan_source_vec, # categorical as batch
+    batch2 = somascan_Sex_vec, # categorical as batch
+    covariates = somascan_Age_vec,  # numeric as co-variate
+    design = somascan_design
+  ) %>%
+  as_tibble(rownames = "Aptamer") %>%  # convert back to tibble
+  gather(-Aptamer, key = LabID, value = adjusted_conc) %>%
+  mutate(adjusted_conc = 2^adjusted_conc) %>% # remove log2 transformation
+  inner_join(somascan_sample_data) %>%
+  rename(Abundance_adj = adjusted_conc)
+#
+
+### 4.2.2 Adjust the metadata for BMI ----
+sample_data_BMI <- somascan_data_meta_data %>% #1,875,960 rows
+  distinct(ParticipantID, LabID, Karyotype, Sex, Age, BMI, Sample_source) %>% # Keeps 450
+  select(-BMI)
+#
+unadj_data_BMI <- somascan_data_meta_data %>% #1,875,960 rows
+  distinct(LabID, BMI) %>% # Keeps 450
+  mutate(feature = "BMI",BMI = log2(BMI)) %>% # need to log2 transform for batch correction
+  pivot_wider(names_from = LabID, values_from = BMI) %>%
+  column_to_rownames(var = "feature") #convert tibble to dataframe
+#
+source_vec_BMI <- unadj_data_BMI %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>% #build tibble
+  inner_join(
+    meta_data %>% select(LabID, Sample_source) 
+  ) %>%
+  pull(Sample_source)
+#
+Sex_vec_BMI <- unadj_data_BMI %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>%
+  inner_join(
+    meta_data %>% select(LabID, Sex) 
+  ) %>%
+  pull(Sex)
+#
+Age_vec_BMI <- unadj_data_BMI %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>%
+  inner_join(
+    meta_data %>% select(LabID, Age) 
+  ) %>%
+  pull(Age)
+#
+design_BMI <- unadj_data_BMI %>% # get batch information, ensuring order matches unadj_data
+  colnames() %>%
+  enframe(name = NULL, value = "LabID") %>%
+  inner_join(
+    meta_data %>% select(LabID, Karyotype)
+  ) %>%
+  model.matrix(~ Karyotype, data = .)
+#
+SourceSexAge_adj_BMI <- unadj_data_BMI %>%
+  limma::removeBatchEffect(
+    batch = source_vec_BMI,  # categorical as batch
+    batch2 = Sex_vec_BMI,  # categorical as batch
+    covariates = Age_vec_BMI, #numeric as co variate
+    design = design_BMI
+  ) %>%
+  as_tibble(rownames = "BMI") %>%  # convert back to tibble
+  gather(-BMI, key = LabID, value = adjusted_bmi) %>%
+  mutate(adjusted_bmi = 2^adjusted_bmi) %>% # remove log2 transformation
+  inner_join(sample_data_BMI) %>%
+  rename(BMI_SourceSexAgeAdj = adjusted_bmi)
+#
+
+## 4.2.3 Get interesting/significant features ----
+top_signif_by_FC <- bind_rows(
+  lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results |> 
+    filter(BHadj_pval<0.1) %>%  
+    arrange(-log2FoldChange) %>%  
+    slice_max(order_by = log2FoldChange, n = 5), # Upregulated
+  lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results |> 
+    filter(BHadj_pval<0.1) %>%  
+    arrange(log2FoldChange) %>%  
+    slice_min(order_by = log2FoldChange, n = 5) # Downregulated
+)
+#
+
+## 4.2.4 Scatter plots ----
+somascan_SourceSexAge_adj %>% #1,875,960 rows
+  inner_join(SourceSexAge_adj_BMI %>% 
+               select(ParticipantID,LabID,BMI_SourceSexAgeAdj) #Get adjusted bmi
+  ) %>% #Keeps 1,875,960 rows
+  filter(Karyotype == "T21") %>% #Keeps 1,454,448 rows
+  filter(Aptamer %in% top_signif_by_FC$Aptamer) %>%  # filter to features of interest
+  mutate(GeneSymbol = fct_relevel(GeneSymbol, top_signif_by_FC$GeneSymbol)) %>%  # control plotting order
+  group_by(GeneSymbol) %>%  #Group by only GeneSymbol
+  mutate(
+    extreme_y = rstatix::is_extreme(log2(Abundance_adj))
+  ) %>%
+  filter(extreme_y == FALSE) %>% # remove extreme outliers
+  mutate(density = getDenCols(BMI_SourceSexAgeAdj, Abundance_adj, transform = TRUE)) %>% 
+  arrange(density) %>% 
+  ungroup() %>%
+  ggplot(aes(BMI_SourceSexAgeAdj, log2(Abundance_adj), color = density)) + #
+  geom_point() +
+  scale_color_viridis_c() +
+  geom_smooth(method = "lm") +
+  facet_wrap(~ GeneSymbol , scales = "free_y", nrow = 2) +# facet per feature; each feature on it's own scale
+  theme(aspect.ratio = 1) + # set fixed aspect ratio
+  labs(title = "Significant plasma proteins by fold-change: T21 vs. BMI",
+       subtitle = paste("Significant associations\nAgeSexSource adj\nextreme outliers are removed"),
+       y = "log2_adj_val",
+       x = "log2_adj_bmi"
+  )
+# save the plot
+ggsave(filename = here("plots", paste0(out_file_prefix, "scatter_T21_top_signif_by_FC", ".pdf")), device = cairo_pdf, width = 15, height = 5, units = "in")
+#
+
+
+# 5 GSEA Hallmarks analysis ----
+set.seed(123)
+#Load the Hallmarks file
+hallmarks <- here("GSEA/h.all.v7.4.symbols.gmt") %>%
+  fgsea::gmtPathways(gmt.file = .)
+#
+#Create a function to run the GSEA on all the aptamers from linear model results
+get_rnk <- function(query) {
+  query %>% 
+    get() %>% 
+    arrange(-abs(FoldChange)) %>% # to ensure keeping "strongest" correlations where dup gene names in next step
+    distinct(GeneSymbol, .keep_all = TRUE) %>% # where Gene_name is not distinct, this keeps the first row of values ( = strongest correlation from above)
+    mutate(rank_metric = log2FoldChange * -log10(pval)) %>% # weighting fold-changes by pval => makes results more stable
+    select(ID = GeneSymbol, r = rank_metric) %>%  # weighting fold-changes by pval
+    #select(ID = GeneSymbol, r = FoldChange) %>% # use raw fold-changes # BHadj_pval
+    arrange(-r) %>%
+    tibble::deframe() # convert to named num vector
+}
+#
+ranksList <- c("lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results")
+#
+names(ranksList) <- paste0(ranksList, "_ranks") # add names
+map(ranksList, get_rnk) %>% # generate rnk vectors for each
+  list2env(envir = .GlobalEnv) # save each rnk vector as object using names
+unweighted_gsea_res_list <- map(names(ranksList), ~ run_fgsea2(geneset = hallmarks, ranks = get(.x), weighted = FALSE))
+names(unweighted_gsea_res_list) <- ranksList
+unweighted_gsea_res_tbl <- bind_rows(unweighted_gsea_res_list, .id = "Query") 
+#
+unweighted_gsea_results <- unweighted_gsea_res_tbl %>% 
+  mutate(pathway = pathway %>%  str_remove("^HALLMARK_") %>% str_replace_all("_", " ") %>% str_to_title()) %>% 
+  arrange(padj) %>% 
+  mutate(leadingEdge = map_chr(leadingEdge, ~ paste(.x, collapse = ", "))) %>% 
+  select(pathway, ES, NES, pval, padj, everything()) 
+#
+unweighted_gsea_results %>% 
+  filter(padj < 0.1)
+#
+
+## 5.1 Export GSEA results ----
+unweighted_gsea_results %>%
+  mutate(across(c(ES, NES), ~round(., 2))) %>%
+  mutate(across(c(pval, padj), ~sprintf("%.2e", .))) %>%
+  write_tsv(file = here("results", paste0(out_file_prefix, "unweighted_gsea_results.txt")))
+#
+
+## 6 GSEA enrichment plots ----
+# Using Mtorc1 Signaling as example here
+plotEnrichment2 (pathway = hallmarks$HALLMARK_MTORC1_SIGNALING,
+                 stats = lm_T21_Aptamer_BMI_Age_Sex_Sample_source_results_ranks,
+                 res = unweighted_gsea_res_tbl %>% select(-Query),
+                 title = "MTORC1 SIGNALING")
+#
+ggsave(filename = here("plots", paste0(out_file_prefix, "enrichment_Mtorc1 Signaling", ".pdf")), device = cairo_pdf, width = 5, height = 4, units = "in")
+
+
+########## End of Script #############
+
+##### Save workspace #####    ----
+################################################################################
+save.image(file = here("rdata", paste0(out_file_prefix, ".RData")), compress = TRUE, safe = TRUE) # saves entire workspace (can be slow)
+
+# Reload the workspace
+# load(here("rdata", paste0(out_file_prefix, ".RData")))
+
+# session_info ----
+date()
+sessionInfo()
+################################################################################
